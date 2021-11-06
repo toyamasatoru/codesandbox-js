688c0ebd6fa7d8c89ff26d48e573dc845f6ef1a1
to-yan <87537864+to-yan@users.noreply.github.com>
2021-11-06 14:09:14 +0900

    let・const基礎

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
-import "./styles.css";
+// var val1 = "var変数";
+// console.log(val1);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// var val1 = "var変数上書き";
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// //letは上書き可能
+// val2 = "let変数を上書き";
+// console.log(val2);
+
+// //letは再宣言不可能
+// let val2 = "let変数を再宣言";
+
+//constは、上書きも再宣言も不可
+
+//const定義したオブジェクトはプロパティの変更が可能
+// const val4 = {
+//   name: "satoru",
+//   age: 31,
+// };
+// val4.name = "さとる";
+// val4.addres = "Saitama";
+// console.log(val4);
+
+//const定義した配列はプロパティの変更が可能
+const val5 = ["dog", "cat"];
+val5[0] = "bird";
+val5.push("mouse");
+console.log(val5);
