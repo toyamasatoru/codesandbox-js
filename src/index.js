06cbc6f833d4964dd7c3f2652926a79bcb2f0a33
to-yan <87537864+to-yan@users.noreply.github.com>
2021-11-06 14:33:43 +0900

    テンプレート文字列・アロー関数

--- src/index.js
+++ src/index.js
@@ -26,7 +26,50 @@
 // console.log(val4);
 
 //const定義した配列はプロパティの変更が可能
-const val5 = ["dog", "cat"];
-val5[0] = "bird";
-val5.push("mouse");
-console.log(val5);
+// const val5 = ["dog", "cat"];
+// val5[0] = "bird";
+// val5.push("mouse");
+// console.log(val5);
+
+/**
+ * テンプレート文字列
+ */
+// const name = "satoru";
+// const age = 31;
+//私の名前はsatoruです。年齢は31歳です。
+
+//従来の方法
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+// console.log(message1);
+
+//テンプレート文字列を用いた方法
+// const message2 = `私の名前は${name}です。年齢は${age}です。`;
+// console.log(message2);
+
+/**
+ * アロー関数
+ */
+//従来の関数
+// function func1(str) {
+//   return str;
+// }
+const func1 = function (str) {
+  return str;
+};
+console.log(func1("func1です"));
+
+//アロー関数
+const func2 = (str) => {
+  return str;
+};
+console.log(func2("func2です"));
+
+const func3 = (num1, num2) => {
+  return num1 + num2;
+};
+
+console.log(func3(10, 20));
+
+//アロー関数（１行ならreturnが省略できる）
+// const func2 = (str) => str;
+// console.log(func2("func2です"));
